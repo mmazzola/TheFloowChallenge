03cf5d5fa8ac3c9eee162e8fd2507261d2c90254
Matteo Mazzola <matteo.mazzola90@gmail.com>
2017-08-24 08:00:34 +0200

    My location showed on Map

--- app/src/main/java/mmazzola/thefloowchallenge/MainActivity.java
+++ app/src/main/java/mmazzola/thefloowchallenge/MainActivity.java
@@ -1,7 +1,12 @@
 package mmazzola.thefloowchallenge;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,6 +15,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    //CONSTANTS
+    private final int PERMISSION_REQUEST_CODE = 1;
+    private final String[] PERMISSION_REQUESTED = {Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +32,34 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        setUpMap();
+    }
+
+    private void setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,PERMISSION_REQUESTED, PERMISSION_REQUEST_CODE );
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        try {
+            switch (requestCode) {
+                case PERMISSION_REQUEST_CODE:
+                    if (grantResults.length > 0 && PackageManager.PERMISSION_GRANTED == grantResults[0]) {
+                        mMap.setMyLocationEnabled(true);
+                    }else{
+                        disableApplication();
+                    }
+            }
+        }catch(SecurityException e){
+            Log.e("PERMISSION ", e.getLocalizedMessage());
+        }
+    }
+
+    private void disableApplication(){
+        //TODO IMPLEMENT
     }
 }
