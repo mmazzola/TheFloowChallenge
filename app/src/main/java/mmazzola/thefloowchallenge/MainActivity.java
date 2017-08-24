d493dbf1e71f4ab6932909fc16c0f26fff31b5c0
Matteo Mazzola <matteo.mazzola90@gmail.com>
2017-08-24 18:20:07 +0200

    Goal 1 - User Position Tracked

--- app/src/main/java/mmazzola/thefloowchallenge/MainActivity.java
+++ app/src/main/java/mmazzola/thefloowchallenge/MainActivity.java
@@ -2,12 +2,18 @@ package mmazzola.thefloowchallenge;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -15,6 +21,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private Location mLastLocation;
 
     //CONSTANTS
     private final int PERMISSION_REQUEST_CODE = 1;
@@ -27,6 +35,34 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        //Iniialize Location Services
+        try {
+            FusedLocationProviderClient mFusionClient = new FusedLocationProviderClient(this);
+
+            LocationCallback mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    mLastLocation = locationResult.getLastLocation();
+                    Log.i("Location", mLastLocation.getLatitude() + " " + mLastLocation.getLongitude());
+                }
+            };
+
+            mFusionClient.requestLocationUpdates(createLocationRequest(), mLocationCallback, Looper.myLooper());
+
+        }catch(SecurityException e){
+                Log.e("PERMISSION ", e.getLocalizedMessage());
+            }
+
+    }
+
+    public LocationRequest createLocationRequest(){
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(2000);
+        return mLocationRequest;
     }
 
     @Override
